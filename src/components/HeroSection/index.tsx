0489b58401eb49b0df53422125fe416a5f286374
akramansaritorinit <akram.ansari@torinit.ca>
2023-04-03 02:03:35 +0530

    minor changes

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -5,16 +5,19 @@ import { useTheme } from "next-themes";
 export default function Example() {
     const { theme } = useTheme();
     return (
-        <div className="dark:bg-black">
-            <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-500/20">
+        <div className="bg-white dark:bg-black">
+            <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-200/20">
                 <div
-                    className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] dark:bg-black  shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+                    className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white dark:bg-black  shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
                     aria-hidden="true"
                 />
                 <div className="h-screen px-6 py-40 sm:py-40 lg:px-8">
                     <div className="flex flex-wrap items-center justify-center gap-10">
                         <div className="md:w-1/2">
-                            <p className="text-lg">
+                            <h1 className="text-5xl font-semibold">
+                                Hello there!
+                            </h1>
+                            <p className="text-xl">
                                 Anim aute id magna aliqua ad ad non deserunt
                                 sunt. Qui irure qui lorem cupidatat commodo.
                                 Elit sunt amet fugiat veniam occaecat fugiat
@@ -25,7 +28,7 @@ export default function Example() {
                             <div className="mt-10 flex items-center gap-x-6">
                                 <a
                                     href="#"
-                                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    className="rounded-md bg-black dark:bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Get started
                                 </a>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,28 +7,29 @@ import {
     XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 const navigation = [
-    { name: "Product", href: "#" },
-    { name: "Features", href: "#" },
-    { name: "Marketplace", href: "#" },
-    { name: "Company", href: "#" },
+    { name: "About", href: "/about" },
+    { name: "Projects", href: "/projects" },
+    { name: "Skills", href: "/skills" },
+    { name: "Contact", href: "/contact" },
 ];
 
 export default function Navbar() {
     const { theme, setTheme } = useTheme();
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-    console.log(theme);
+
     return (
-        <header className="absolute top-0 z-50">
+        <header className="sticky top-0 z-50 dark:bg-black">
             <nav
                 className="flex items-center justify-between p-6 lg:px-8"
                 aria-label="Global"
             >
-                <div className="flex md:hidden">
+                <div className="w-full flex justify-end md:hidden">
                     <button
                         type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+                        className="-m-2.5 rounded-md p-2.5"
                         onClick={() => setMobileMenuOpen(true)}
                     >
                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
@@ -36,13 +37,13 @@ export default function Navbar() {
                 </div>
                 <div className="hidden md:flex md:gap-x-12">
                     {navigation.map((item) => (
-                        <a
+                        <Link
                             key={item.name}
                             href={item.href}
-                            className="text-sm font-semibold leading-6 dark:text-green-400"
+                            className="text-lg font-semibold border-b-2 border-transparent dark:text-red-500  hover:border-red-500"
                         >
                             {item.name}
-                        </a>
+                        </Link>
                     ))}
                 </div>
                 <div className="hidden md:flex">
@@ -69,11 +70,25 @@ export default function Navbar() {
                 onClose={setMobileMenuOpen}
             >
                 <div className="fixed inset-0 z-50" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-700 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex justify-end">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-100 dark:bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex justify-end gap-3">
+                        <button
+                            className="border p-1 rounded-full"
+                            onClick={() =>
+                                theme === "light"
+                                    ? setTheme("dark")
+                                    : setTheme("light")
+                            }
+                        >
+                            {theme === "dark" ? (
+                                <MoonIcon className="h-5 w-5" />
+                            ) : (
+                                <SunIcon className="h-5 w-5" />
+                            )}
+                        </button>
                         <button
                             type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5"
                             onClick={() => setMobileMenuOpen(false)}
                         >
                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
@@ -83,22 +98,14 @@ export default function Navbar() {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
-                                    <a
+                                    <Link
                                         key={item.name}
                                         href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-red-500"
                                     >
                                         {item.name}
-                                    </a>
+                                    </Link>
                                 ))}
-                            </div>
-                            <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Log in
-                                </a>
                             </div>
                         </div>
                     </div>
