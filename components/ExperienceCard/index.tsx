1710da209bd042ca58b76514d88b5b1bc16d0e77
akramansari1433 <akramansari1433@gmail.com>
2023-07-16 12:02:42 +0530

    minor fix

--- components/ExperienceCard/index.tsx
+++ components/ExperienceCard/index.tsx
@@ -35,53 +35,38 @@ const demoExperience = {
 
 export default function ExperienceCard() {
     return (
-        <article className=" flex drop-shadow-xl flex-col rounded-3xl items-center space-y-0 flex-shrink-0 w-72  md:w-[600px] xl:w-[700px] snap-center bg-gradient-to-tr from-white  to-green-500/20 p-5 md:p10 hover:opacity-100 opacity-100 cursor-pointer transition-opacity duration-200 ">
+        <article className="flex drop-shadow-xl flex-col rounded-3xl items-center flex-shrink-0 w-72  md:w-[600px] xl:w-[700px] snap-center bg-gradient-to-tr from-gray-200  to-green-500/40 p-5 md:p-8 hover:opacity-80 transition-opacity duration-200 ">
             <motion.img
                 initial={{ opacity: 0, y: -100 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 transition={{ duration: 1.2 }}
-                className=" md:invisible xl:visible md:h-0 w-28 h-28 md:w-0 rounded-full xl:w-[150px] xl:h-[150px] mb-2 object-cover object-center"
+                className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-contain object-center"
                 src={demoExperience.companyImage.url}
                 alt=""
             />
             <div className="w-full px-0 md:px-10">
-                <div className=" md:flex md:justify-between items-center">
-                    <div>
-                        <h4 className="text-lg md:text-3xl font-light text-black">{demoExperience.jobTitle}</h4>
-                        <p className="font-bold text-md md:text-2xl  mt-1 text-lightGreen">{demoExperience.company}</p>
-                        <div className="flex space-x-2 my-2">
-                            {demoExperience?.technologies.map((technology) => (
-                                <img
-                                    key={technology._id}
-                                    className="h-10 w-10 rounded-full object-cover"
-                                    src={technology.image.url}
-                                    alt=""
-                                />
-                            ))}
-                        </div>
-                    </div>
-                    <motion.img
-                        initial={{ opacity: 0, y: -100 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        viewport={{ once: true }}
-                        transition={{ duration: 1.2 }}
-                        className="invisible md:visible xl:invisible xl:h-0 xl:w-0 h-0 w-0 md:h-28 md:w-36  rounded-full mb-0 object-contain object-center"
-                        src={demoExperience.companyImage.url}
-                        alt=""
-                    />
+                <h4 className="text-lg md:text-3xl font-light text-black">{demoExperience.jobTitle}</h4>
+                <p className="font-bold text-md md:text-2xl  mt-1 text-lightGreen">{demoExperience.company}</p>
+                <div className="flex space-x-2 my-2">
+                    {demoExperience?.technologies.map((technology) => (
+                        <img
+                            key={technology._id}
+                            className="h-10 w-10 rounded-full object-cover"
+                            src={technology.image.url}
+                            alt=""
+                        />
+                    ))}
                 </div>
-                <p className="uppercase py-2 md:py-5 text-gray-500 text-sm md:text-lg">
+                <p className="uppercase py-2 md:py-3 text-gray-500 text-sm md:text-lg">
                     {new Date(demoExperience?.dateStarted).toDateString()} -{" "}
                     {demoExperience.isCurrentlyWorkingHere
                         ? "Present"
                         : new Date(demoExperience?.dateEnded).toDateString()}
                 </p>
             </div>
-            <ul className="px-0 md:px-10 list-disc  text-black space-y-2 pr-5 text-justify ml-0 text-sm md:text-lg pl-5 overflow-y-scroll scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-darkGreen/80">
-                {demoExperience?.points.map((point, i) => (
-                    <li key={i}>{point}</li>
-                ))}
+            <ul className="px-5 md:px-10 list-disc text-black space-y-2 text-justify text-sm md:text-lg overflow-y-scroll scrollbar-thin">
+                {demoExperience?.points.map((point, i) => <li key={i}>{point}</li>)}
             </ul>
         </article>
     );
--- components/WorkExperience/index.tsx
+++ components/WorkExperience/index.tsx
@@ -8,13 +8,17 @@ export default function WorkExperience() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="h-screen  flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+            className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
         >
             <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-700 dark:text-gray-200 text-xl md:text-2xl font-medium">
                 Experience
             </h3>
 
-            <div className="w-screen h-3/4 md:h-2/3 md:w-full text-left pb-5 md:pb-10 flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-darkGreen/80">
+            <div className="w-screen md:w-full h-flex max-h-[75%] flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin">
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
                 <ExperienceCard />
             </div>
         </motion.div>
