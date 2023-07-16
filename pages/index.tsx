e793f5e49a1ad61c25367e7aa2ca43d6894d97d9
akramansari1433 <akramansari1433@gmail.com>
2023-07-16 14:08:26 +0530

    minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,32 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import { fetchExperiences } from "@/utils/fetchExperience";
+import { fetchPageInfo } from "@/utils/fetchPageInfo";
+import { fetchProjects } from "@/utils/fetchProjects";
+import { fetchSkills } from "@/utils/fetchSkills";
+import { fetchSocials } from "@/utils/fetchSocials";
 import { ChevronUpIcon } from "@heroicons/react/24/solid";
+import { GetStaticProps } from "next";
 import Link from "next/link";
 import React from "react";
 
-export default function Home() {
+type Props = {
+    pageInfo: PageInfo;
+    experiences: Experience[];
+    skills: Skill[];
+    projects: Project[];
+    socials: Social[];
+};
+
+export default function Home({ pageInfo, experiences, projects, skills, socials }: Props) {
     return (
         <div
             className="bg-gray-200 dark:bg-gray-900 text-black h-screen snap-y snap-mandatory
             overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-darkGreen/80"
         >
             <Header />
+
             {/* Hero */}
             <section id="hero" className="snap-start">
                 <Hero />
@@ -36,6 +51,7 @@ export default function Home() {
                 <Skills />
             </section>
 
+            {/* Projects */}
             <section className="snap-start" id="projects">
                 <Projects />
             </section>
@@ -45,15 +61,32 @@ export default function Home() {
                 <ContactMe />
             </section>
 
-            <Link href="#hero">
-                <footer className="sticky bottom-5 w-full cursor-pointer">
-                    <div className="flex items-center justify-center">
-                        <div className="p-2 border bg-gray-400 dark:bg-transparent rounded-full">
-                            <ChevronUpIcon className="h-6 w-6 text-white animate-pulse" />
-                        </div>
-                    </div>
-                </footer>
-            </Link>
+            <footer className="sticky bottom-5 w-full cursor-pointer">
+                <div className="flex items-center justify-center">
+                    <Link href="#hero" className="p-2 border bg-gray-400 dark:bg-transparent rounded-full">
+                        <ChevronUpIcon className="h-6 w-6 text-white animate-pulse" />
+                    </Link>
+                </div>
+            </footer>
         </div>
     );
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+    const pageInfo = await fetchPageInfo();
+    const experiences = await fetchExperiences();
+    const skills = await fetchSkills();
+    const projects = await fetchProjects();
+    const socials = await fetchSocials();
+
+    return {
+        props: {
+            pageInfo,
+            experiences,
+            skills,
+            projects,
+            socials,
+        },
+        revalidate: 10,
+    };
+};
