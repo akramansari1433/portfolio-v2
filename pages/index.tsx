7cf2264b2e121c400e8ba974e34c8c3505e351fb
akramansari1433 <akramansari1433@gmail.com>
2023-07-16 22:50:30 +0530

    build fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,10 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
-import { fetchExperiences } from "@/utils/fetchExperience";
-import { fetchPageInfo } from "@/utils/fetchPageInfo";
-import { fetchProjects } from "@/utils/fetchProjects";
-import { fetchSkills } from "@/utils/fetchSkills";
-import { fetchSocials } from "@/utils/fetchSocials";
+import { client } from "@/sanity/lib/client";
 import { ChevronUpIcon } from "@heroicons/react/24/solid";
 import { GetStaticProps } from "next";
+import { groq } from "next-sanity";
 import Link from "next/link";
 import React from "react";
 
@@ -73,11 +70,36 @@ export default function Home({ pageInfo, experiences, projects, skills, socials 
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-    const pageInfo = await fetchPageInfo();
-    const experiences = await fetchExperiences();
-    const skills = await fetchSkills();
-    const projects = await fetchProjects();
-    const socials = await fetchSocials();
+    const socialsQuery = groq`
+    *[_type == 'social']`;
+
+    const pageInfoQuery = groq`
+    *[_type == 'pageInfo'][0]
+    `;
+
+    const skillQuery = groq`
+    *[_type == 'skill']
+    `;
+
+    const projectQuery = groq`
+    *[_type == 'project'] {
+      ...,
+      technologies[]->
+    }
+    `;
+
+    const experienceQuery = groq`
+    *[_type == 'experience'] {
+      ...,
+      technologies[]->
+    }
+    `;
+
+    const socials: Social[] = await client.fetch(socialsQuery);
+    const experiences: Experience[] = await client.fetch(experienceQuery);
+    const skills: Skill[] = await client.fetch(skillQuery);
+    const projects: Project[] = await client.fetch(projectQuery);
+    const pageInfo: PageInfo = await client.fetch(pageInfoQuery);
 
     return {
         props: {
