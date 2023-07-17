6e2784f67bfa140e074c404007b38ed1f0284221
akramansari1433 <akramansari1433@gmail.com>
2023-07-18 01:29:37 +0530

    minor fix

--- components/Skill/index.tsx
+++ components/Skill/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
 
 type Props = {
     skill: Skill;
@@ -10,14 +11,20 @@ type Props = {
 export default function Skill({ directionLeft, skill }: Props) {
     return (
         <div className="group relative flex cursor-pointer">
-            <motion.img
+            <motion.div
                 initial={{ x: directionLeft ? -80 : 80, opacity: 0 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1 }}
-                className="rounded-full p-1 border-2 border-green-500 object-cover w-16 h-16 md:w-20 md:h-20 filter group-hover:grayscale transition duration-300 ease-in-out"
-                src={urlForImage(skill.image.asset).url()}
-                alt={skill.title}
-            />
+            >
+                <Image
+                    className="rounded-full p-1 border-2 border-green-500 object-contain w-16 h-16 md:w-20 md:h-20 filter group-hover:grayscale transition duration-300 ease-in-out"
+                    src={urlForImage(skill.image.asset).url()}
+                    alt={skill.title}
+                    height={100}
+                    width={100}
+                />
+            </motion.div>
+
             <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 md:w-20 md:h-20 rounded-full z-0">
                 <div className="flex items-center justify-center h-full">
                     <p className="text-xl md:text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
